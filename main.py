4a077e36fe118126ecece29acd5c02ebddf47079
Jesús R <jde.ram9@gmail.com>
2021-12-13 13:29:38 -0600

    Print functions for all and for a specific file

--- main.py
+++ main.py
@@ -11,9 +11,14 @@ def cli():
 @click.command(name = 'printfile')
 @click.option('--file')
 @click.argument('file')
-def printfiles(file):
+def printfile(file):
     print_file(file)
 
+@click.command(name = 'printall')
+def printfiles():
+    print_files(get_files())
+
+cli.add_command(printfile)
 cli.add_command(printfiles)
 
 if __name__ == '__main__':
